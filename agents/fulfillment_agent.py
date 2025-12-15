2d92074389d4f4264e9a4af86168289c4346d9a4
Peethambari123 <preethimanavarti550@gmail.com>
2025-12-15 18:45:50 +0530

    Update fulfillment_agent.py

--- agents/fulfillment_agent.py
+++ agents/fulfillment_agent.py
@@ -1,1 +1,10 @@
+class FulfillmentAgent:
+    def run(self, payment):
+        if payment["status"] == "success":
+            return {
+                "message": "🚚 Order confirmed! Delivery in 3–5 days."
+            }
+        return {
+            "message": "❌ Payment failed. Please try another method."
+        }
 
