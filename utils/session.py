ac49309d4318d0578ed92d44e80689909f78b640
Peethambari123 <preethimanavarti550@gmail.com>
2025-12-15 18:43:05 +0530

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+def init_session():
+    if "context" not in st.session_state:
+        st.session_state.context = {
+            "session_id": "SESSION_001",
+            "user_id": "USER_01",
+            "channel": "web",
+            "cart": [],
+            "conversation": []
+        }
+    return st.session_state.context
